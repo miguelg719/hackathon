72921c6f05548916619da05f040c83a0d7ebf093
miguelg719 <miguelg71921@gmail.com>
2021-10-17 04:00:21 -0700

    first commit

---
+++ transporter/back-end/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema
+let User = new Schema({
+   name: {
+      type: String
+   },
+   lastName: {
+      type: String
+   },
+   email: {
+      type: String
+   },
+}, {
+   collection: 'users'
+})
+
+module.exports = mongoose.model('User', User)
